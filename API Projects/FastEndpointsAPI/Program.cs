8db09485b412201e08345f9a0ee723bcb875f496
fu <fu.xu@gmail.com>
2022-08-01 16:07:33 +0100

    fastendpointapi

--- API Projects/FastEndpointsAPI/Program.cs
+++ API Projects/FastEndpointsAPI/Program.cs
@@ -1,13 +1,27 @@
+using BackendData;
+using BackendData.DataAccess;
+using FastEndpoints;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=database.sqlite;Cache=Shared";
+var connection = new SqliteConnection(connectionString);
+connection.Open();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add services to the container.
+builder.Services.AddFastEndpoints();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connection)); // will be created in web project root
+builder.Services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+await EnsureDb(app.Services, app.Logger);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,7 +33,16 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
+app.UseFastEndpoints();
 
 app.Run();
+
+async Task EnsureDb(IServiceProvider services, ILogger logger)
+{
+    using var db = services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
+    if (db.Database.IsRelational())
+    {
+        logger.LogInformation("Ensuring database exists and is up to date at connection string '{connectionString}'", connectionString);
+        await db.Database.MigrateAsync();
+    }
+}
