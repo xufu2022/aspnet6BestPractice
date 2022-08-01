db1c2e69fea3d01860fa0ef11eaf8ecd1c68e3a8
fu <fu.xu@gmail.com>
2022-08-01 15:55:40 +0100

    minimal

--- API Projects/minimal/Program.cs
+++ API Projects/minimal/Program.cs
@@ -1,25 +1,89 @@
+using BackendData;
+using BackendData.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using minimal.Authors;
+using MinimalApi.Endpoint.Extensions;
+using Swashbuckle.AspNetCore.Annotations;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// MinimalApi.Endpoint registration
+builder.Services.AddEndpoints();
 
-builder.Services.AddControllers();
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=database.sqlite;Cache=Shared";
+builder.Services.AddSqlite<AppDbContext>(connectionString);
+builder.Services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
+
+builder.Services.AddMvcCore(); // pull in default model binders
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsMetadataProviderApiExplorer();
+builder.Services.AddSwaggerGen(config => config.EnableAnnotations());
 
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 var app = builder.Build();
-
+await EnsureDb(app.Services, app.Logger);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler(new ExceptionHandlerOptions
+    {
+        ExceptionHandlingPath = "/error",
+        AllowStatusCode404Response = true
+    });
+}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapEndpoints();
+// extension method
+app.RegisterAuthorUpdateEndpoint();
+
+app.MapGet("/authors", async (AppDbContext db) => await db.Authors.ToListAsync())
+    .WithName("ListAuthors")
+    .WithTags("AuthorsApi");
+
+app.MapDelete("/authors/{id}",
+        [SwaggerOperation(
+            Summary = "Deletes an author.",
+            Description = "Deletes an author, which must exist, otherwise NotFound is returned.")]
+        [SwaggerResponse(204, "Success")]
+        [SwaggerResponse(404, "Not Found")]
+        async (IAsyncRepository<Author> repo, int id) =>
+        {
+            var author = await repo.GetByIdAsync(id, cancellationToken: default);
+            if (author == null) return Results.NotFound();
+            await repo.DeleteAsync(author, cancellationToken: default);
+            return Results.NoContent();
+        })
+    .WithName("DeleteAuthor")
+    .WithTags("AuthorsApi");
 
 app.Run();
+
+async Task EnsureDb(IServiceProvider services, ILogger logger)
+{
+    using var db = services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
+    if (db.Database.IsRelational())
+    {
+        logger.LogInformation("Ensuring database exists and is up to date at connection string '{connectionString}'", connectionString);
+        await db.Database.MigrateAsync();
+    }
+}
+
+public class AuthorDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string TwitterAlias { get; set; }
+}
